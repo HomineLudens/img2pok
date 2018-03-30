da5e97bddefaf3db24c7023dd69e2cd74aa62f77
Effer <f.rivato@gmail.com>
2018-03-30 19:31:33 +0200

    V0.9 Fix pixel perfect scaling

--- img2pok/Form1.cs
+++ img2pok/Form1.cs
@@ -577,6 +577,7 @@ namespace img2pok
         private void DrawPictureBoxPixelPerfect(PictureBox pb, PaintEventArgs e)
         {
             e.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+            e.Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.Half;
             e.Graphics.Clear(pb.BackColor);
             if (pb.Image != null)
             {
@@ -584,10 +585,10 @@ namespace img2pok
                 Size targetSize = pb.Size;
                 float scale = Math.Min((float)targetSize.Width / sourceSize.Width, (float)targetSize.Height / sourceSize.Height);
                 var rect = new RectangleF();
-                rect.Width = scale * sourceSize.Width;
-                rect.Height = scale * sourceSize.Height;
-                rect.X = (targetSize.Width - rect.Width) / 2;
-                rect.Y = (targetSize.Height - rect.Height) / 2;
+                rect.Width = scale * (float)sourceSize.Width;
+                rect.Height = scale * (float)sourceSize.Height;
+                rect.X = (targetSize.Width - rect.Width) / 2.0f;
+                rect.Y = (targetSize.Height - rect.Height) / 2.0f;
                 e.Graphics.DrawImage(pb.Image, rect);
             }
         }
